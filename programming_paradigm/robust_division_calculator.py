c7d1066d9dbc3d4ea669b1d887bd7855e04e5410
MARTIN-MONYEBODI <martinmonyebodi@gmail.com>
2025-08-20 11:58:58 +0200

    Add robust calculator with eoor handling

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,10 @@
+def safe_divide(numerator, denominator):
+    
+    try:
+        result = float(numerator) / float(denominator)
+        return result
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+        
+    except ValueError:
+        print("Error: Please enter numeric values only.")
