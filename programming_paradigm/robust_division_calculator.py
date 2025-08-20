fddfbd26a2b2ada646bbfd209ebcf840dabc552b
MARTIN-MONYEBODI <martinmonyebodi@gmail.com>
2025-08-20 12:11:31 +0200

    Fix the return message of the try block to show one decimal place

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,7 +2,7 @@ def safe_divide(numerator, denominator):
     
     try:
         result = float(numerator) / float(denominator)
-        return f"The result of the division is {result:.2f}"
+        return f"The result of the division is {result}"
     except ZeroDivisionError:
         return f"Error: Cannot divide by zero."
         
